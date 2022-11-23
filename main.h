c246307809b1388549c5892acefbf0d70db1801c
yusufyassir <yusufyassir0@gmail.com>
2022-11-23 02:04:19 -0800

    dfghjk

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef PRINT_F
+#define PRINT_F
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+/*Main functions*/
+int parser(const char *format, conver_t f_list[], va_list arg_list);
+int _printf(const char *format, ...);
+#endif
